b97dabe35960953f165d7117b2aeba26ba00a817
hmos9c <hmos9c@gmail.com>
2022-12-07 21:03:38 +0700

    Membuat Project Java Database Todolist

---
+++ src/main/java/hmos9c/todolist/App.java
@@ -0,0 +1,13 @@
+package hmos9c.todolist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
